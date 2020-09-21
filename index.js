418a49eeba5b3e5d6b668f4e0dd688fee5aa25c9
LolWastedJS <ml2dofficial@gmail.com>
2020-09-21 02:29:51 -0700

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint",
   parserOptions: {
     ecmaVersion: 2019,
